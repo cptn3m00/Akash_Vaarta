25db1ba953c48d4280be0ffb195927eefd4a7443
cptn3m00 <akashtjsamajdar@gmail.com>
2024-07-13 16:14:30 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,49 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Akash';
+
+export const experiences = [
+    {
+        'course': 'AWS Academy',
+        'role': 'Cloud Foundations Trainee',
+        'duration': 'July 2024 - Present',
+        'logo': 'https://media.licdn.com/dms/image/D4D12AQEmC2CSTK0unw/article-cover_image-shrink_423_752/0/1691964348159?e=1726099200&v=beta&t=JGOABcmGp-KmJdSqeLgcaSey60h8qzTsgwdkr933r2Y',
+        'points': [
+            'Gained in-depth understanding of cloud computing concepts: Acquired foundational knowledge about cloud computing, its benefits, and key service models.',
+            'Learned about core AWS services like EC2, S3, RDS, and Lambda, understanding their features and use cases, And Gained practical experience using the AWS Management Console',
+            'Studied cloud architecture principles: Learned the fundamental principles of cloud architecture, including scalability, high availability, and fault tolerance, to design resilient and efficient cloud solutions. 📈.',
+            'Engaged in practical exercises and labs to apply theoretical knowledge to real-world scenarios, enhancing problem-solving skills and practical understanding. ⚙️.'
+        ],
+        'url': 'https://aws.amazon.com/about-aws/?nc2=h_header',
+    },
+    {
+        'company': 'Semester Project',
+        'role': 'Project Head',
+        'duration': 'Jan 2023 - March 2023',
+        'logo': 'https://images.squarespace-cdn.com/content/v1/57713a8e2994cae381dd86fe/1510405002757-FP2W9HV8ZA0HP2IF3O6A/SOA-PNG.png',
+        'points': [
+            'Worked on the application backend, making it more usable and improving its overall functionality📱.'
+        ],
+        // 'url': 'https://www.att.com/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_90wb9t5';
+export const EMAIL_JS_TEMPLATE_ID = 'template_8l87rew';
+export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86N';
