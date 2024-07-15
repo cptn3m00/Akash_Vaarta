14a273e30442bbee4ee0a8df23798faf7447a674
Akash <akashtjsamajdar@gmail.com>
2024-07-15 20:24:41 +0530

    new footer setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,36 +2,29 @@ import React from 'react';
 import { FaGithub, FaTwitter } from 'react-icons/fa';
 import { AiFillMediumCircle, AiFillLinkedin } from 'react-icons/ai';
 import { FiMail } from 'react-icons/fi';
-
+import './Footer.css';
 
 const Footer = () => {
-	const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
 
-	return (
-		<div className='bg-slate-900 p-4' style={{ cursor: 'default' }}>
-			<div className='flex flex-wrap justify-center sm:justify-between items-center gap-4'>
-				<p className='text-white text-center w-full sm:w-auto font-light'>© {year} Akash. All rights reserved.</p>
-				<div className='text-white flex justify-around sm:w-[250px] w-full'>
-					
-					<a href="mailto:akashtjsamajdar@gmail.com" className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
-						<FiMail className='text-xl' />
-					</a>
-					{/* <a href="https://twitter.com/Nithin80875999" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
-						<FaTwitter className='text-xl' />
-					</a> */}
-					<a href="https://www.linkedin.com/in/akash-samajdar-298107188/" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
-						<AiFillLinkedin className='text-xl' />
-					</a>
-					<a href="https://https://github.com/cptn3m00" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }} >
-						<FaGithub className='text-xl' />
-					</a>
-					{/* <a href="https://medium.com/@goudnithin77" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
-						<AiFillMediumCircle className='text-xl' />
-					</a> */}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <footer className='footer bg-slate-900 p-4'>
+      <div className='footer-content flex flex-wrap justify-center sm:justify-between items-center gap-4'>
+        <p className='footer-text text-white text-center w-full sm:w-auto font-light'>© {year} Akash. All rights reserved.</p>
+        <div className='footer-icons text-white flex justify-around sm:w-auto w-full'>
+          <a href="mailto:akashtjsamajdar@gmail.com" className='icon-link'>
+            <FiMail className='text-xl' />
+          </a>
+          <a href="https://www.linkedin.com/in/akash-samajdar-298107188/" target='_blank' className='icon-link'>
+            <AiFillLinkedin className='text-xl' />
+          </a>
+          <a href="https://github.com/cptn3m00" target='_blank' className='icon-link'>
+            <FaGithub className='text-xl' />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
